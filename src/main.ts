1e6a0f193cd485e24f93929df3bc14f67f9533cc
Tali <talikulangiave7@gmail.com>
2025-11-08 02:25:36 +0200

    #

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { routes } from './app/app.routes'; s
+import { routes } from './app/app.routes'; 
 
 bootstrapApplication(App, {
   ...appConfig, 
