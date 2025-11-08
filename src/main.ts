d6f3edaaf1a0dc9ec8ad3393c15c99e0cf96e046
Tali <talikulangiave7@gmail.com>
2025-11-08 02:25:20 +0200

    #Adding

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { routes } from './app/app.routes'; s
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  ...appConfig, 
+  providers: [
+    ...(appConfig.providers || []), 
+    provideRouter(routes, withHashLocation()) 
+  ]
+})
+.catch((err) => console.error(err));
